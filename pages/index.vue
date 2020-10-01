b84ddbd2a8529e01a4da028b5bfee6b422e5999f
RaduCStefanescu <stefanescuradu@gmail.com>
2020-10-01 13:56:36 +0300

    Remove decision tree

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,6 @@
   <div class="container">
     <div class="row">
       <div class="col-12 mt-4 mb-5">
-        <VotersGuide />
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12 mt-4 mb-5">
         <h2 class="h4 text-center">
           {{ $t('pollingStationSearch.question') }}
         </h2>
