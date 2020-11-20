ab8867930e230c888fbcbbf52760ab30ccbeb976
Utwo <mihai.legat@gmail.com>
2020-11-20 14:51:19 +0200

    Update ro content

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,41 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 mt-4 mb-5">
-        <VotersGuide />
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mt-4 mb-5">
+          <VotersGuide />
+        </div>
       </div>
     </div>
-    <div class="row mt-4 mb-5">
-      <div class="col-lg-4">
-        <h4>{{ $t('exceptionalCases.title1') }}</h4>
-        <div v-html="$t('exceptionalCases.content1')"></div>
-      </div>
-      <div class="col-lg-4">
-        <h4>{{ $t('exceptionalCases.title2') }}</h4>
-        <div v-html="$t('exceptionalCases.content2')"></div>
-      </div>
-      <div class="col-lg-4">
-        <h4>{{ $t('exceptionalCases.title3') }}</h4>
-        <div v-html="$t('exceptionalCases.content3')"></div>
+    <div class="bg-light">
+      <div class="container mt-4 mb-5 py-4">
+        <div class="row">
+          <div class="col-lg-4">
+            <h4>{{ $t('exceptional_cases.case1_title') }}</h4>
+            <div v-html="$t('exceptional_cases.case1_text')"></div>
+          </div>
+          <div class="col-lg-4">
+            <h4>{{ $t('exceptional_cases.case2_title') }}</h4>
+            <div v-html="$t('exceptional_cases.case2_text')"></div>
+          </div>
+          <div class="col-lg-4">
+            <h4>{{ $t('exceptional_cases.case3_title') }}</h4>
+            <div v-html="$t('exceptional_cases.case3_text')"></div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12 mt-4 mb-5">
-        <h2 class="h4 text-center">
-          {{ $t('pollingStationSearch.question') }}
-        </h2>
-        <p class="text-center">
-          {{ $t('pollingStationSearch.callToAction') }}
-        </p>
-        <PollingStationSearchGMaps />
+    <div class="container mt-4 mb-5">
+      <div class="row">
+        <div class="col-12 mt-4 mb-5">
+          <h2 class="h4 text-center">
+            {{ $t('pollingStationSearch.question') }}
+          </h2>
+          <p class="text-center">
+            {{ $t('pollingStationSearch.callToAction') }}
+          </p>
+          <PollingStationSearchGMaps />
+        </div>
       </div>
     </div>
   </div>
